39a5559cf860253abdb1f2da8fbe1f97df5ea855
priya-prasanth <priyaprasanths168.com>
2023-12-12 16:22:03 +0530

    changes done in DB

--- index.js
+++ index.js
@@ -1,148 +1,147 @@
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 dotenv.config();
-import connectDB from "./Database/DbConfig.js"
-import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
-import cookieParser from "cookie-parser"
-import UserModel from "./Models/User.js"
-import nodemailer from "nodemailer"
+import connectDB from "./Database/dbConfig.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
+import UserModel from "./Models/User.js";
+import nodemailer from "nodemailer";
 
-
-const app = express()
-app.use(express.json())
-app.use(cors({
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
     origin: ["http://localhost:5173"],
-    methods:["GET","POST"],
-    credentials:true
-}))
-app.use(cookieParser())
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
-connectDB()
+connectDB();
 
 const verifyUser = (req, res, next) => {
-    const token = req.cookies.token;
-    console.log(token);
+  const token = req.cookies.token;
+  console.log(token);
+};
 
-}
+app.get("/home", verifyUser, (req, res) => {});
 
-app.get('/home',verifyUser, (req, res) => {
-    
-})
-
-app.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    UserModel.findOne({ email: email })
-        .then(user => {
-            if (user) {
-                bcrypt.compare(password, user.password, (err, response) => {
-                    if (response) {
-                        const token = jwt.sign({ email: user.email, role:user.role }, "jwt-secret-key", { expiresIn: "1d" })
-                        res.cookie("token", token);
-                        return res.json({ Status:"Success", role: user.role });
-                    } else {
-                       return res.json("The password is incorrect")
-                    }
-                })
-            } else {
-                return res.json("No record existed")
-            }
-        })
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  UserModel.findOne({ email: email }).then((user) => {
+    if (user) {
+      bcrypt.compare(password, user.password, (err, response) => {
+        if (response) {
+          const token = jwt.sign(
+            { email: user.email, role: user.role },
+            "jwt-secret-key",
+            { expiresIn: "1d" }
+          );
+          res.cookie("token", token);
+          return res.json({ Status: "Success", role: user.role });
+        } else {
+          return res.json("The password is incorrect");
+        }
+      });
+    } else {
+      return res.json("No record existed");
+    }
+  });
 });
 const varifyUser = (req, res, next) => {
-    const token = req.cookies.token;
-    if (!token) {
-        return res.json("Token is missing")
-    } else {
-        jwt.verify(token, "jwt-secret-key", (err, decoded) => {
-            if (err) {
-                return res.json("Error with token")
-            } else {
-                if (decoded.role === "admin") {
-                    next()
-                } else {
-                    return res.json("not admin")
-                }
-            }
-                
-        })
-    }
-}
-
-app.get('/dashboard', varifyUser, (req, res) => {
-    res.json("Success")
-})
-
-
-
-app.post('/register', (req, res) => {
-    const { name, email, password } = req.body;
-    bcrypt.hash(password, 10)
-        .then(hash => {
-            UserModel.create({ name, email, password: hash })
-                .then(user => res.json("Success"))
-                .catch(err => res.json(err));
-        }).catch(err => res.json(err))
-   
-})
-
-const port = process.env.PORT
-app.listen(port, () => {
-    console.log("My app is listening at-",port);
-})
-
-app.post('/forgot-password', (req, res) => {
-    const { email } = req.body;
-    UserModel.findOne({ email: email })
-        .then(user => {
-            if (!user) {
-                return res.send({ Status: "User not existed" })
-            }
-            const token = jwt.sign({ id: user._id }, "jwt_secret_key", { expiresIn: "1d" })
-            var transporter = nodemailer.createTransport({
-              service: "gmail",
-              auth: {
-                user: "priyaprasanth168@gmail.com",
-                pass: "qdxh lkky ijre lryh",
-              },
-            });
-
-            var mailOptions = {
-              from: "priyaprasanth168@gmail.com",
-              to: "priyaprasanth168@gmail.com",
-              subject: "Reset password Link",
-              text: `http://localhost:5173/reset_password/${user._id}/${token}`
-            };
-
-            transporter.sendMail(mailOptions, function (error, info) {
-              if (error) {
-                console.log(error);
-              } else {
-                 // console.log('Email sent:' + info.respnse);
-                return res.send({Status:"Success"})
-              }
-            });
-    })
-})
-
-app.post('/reset-password/:id/:token', (req, res) => {
-    const { id, token } = req.params
-    const { password } = req.body
-    
-
-    jwt.verify(token, "jwt_secret_key", (err, decoded) => {
-        if (err) {
-            return res.json({Status:"Error with token"})
+  const token = req.cookies.token;
+  if (!token) {
+    return res.json("Token is missing");
+  } else {
+    jwt.verify(token, "jwt-secret-key", (err, decoded) => {
+      if (err) {
+        return res.json("Error with token");
+      } else {
+        if (decoded.role === "admin") {
+          next();
         } else {
-            bcrypt
-              .hash(password, 10)
-              .then((hash) => {
-                UserModel.findByIdAndUpdate({ _id: id }, { password: hash })
-                  .then((u) => res.send({ Status: "Success" }))
-                  .catch((err) => res.send({ Status: err }));
-              })
-              .catch((err) => res.send({ Status: err }));
+          return res.json("not admin");
         }
+      }
+    });
+  }
+};
+
+app.get("/dashboard", varifyUser, (req, res) => {
+  res.json("Success");
+});
+
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+  bcrypt
+    .hash(password, 10)
+    .then((hash) => {
+      UserModel.create({ name, email, password: hash })
+        .then((user) => res.json("Success"))
+        .catch((err) => res.json(err));
     })
-})
+    .catch((err) => res.json(err));
+});
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log("My app is listening at-", port);
+});
+
+app.post("/forgot-password", (req, res) => {
+  const { email } = req.body;
+  UserModel.findOne({ email: email }).then((user) => {
+    if (!user) {
+      return res.send({ Status: "User not existed" });
+    }
+    const token = jwt.sign({ id: user._id }, "jwt_secret_key", {
+      expiresIn: "1d",
+    });
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "priyaprasanth168@gmail.com",
+        pass: "qdxh lkky ijre lryh",
+      },
+    });
+
+    var mailOptions = {
+      from: "priyaprasanth168@gmail.com",
+      to: "priyaprasanth168@gmail.com",
+      subject: "Reset password Link",
+      text: `http://localhost:5173/reset_password/${user._id}/${token}`,
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        // console.log('Email sent:' + info.respnse);
+        return res.send({ Status: "Success" });
+      }
+    });
+  });
+});
+
+app.post("/reset-password/:id/:token", (req, res) => {
+  const { id, token } = req.params;
+  const { password } = req.body;
+
+  jwt.verify(token, "jwt_secret_key", (err, decoded) => {
+    if (err) {
+      return res.json({ Status: "Error with token" });
+    } else {
+      bcrypt
+        .hash(password, 10)
+        .then((hash) => {
+          UserModel.findByIdAndUpdate({ _id: id }, { password: hash })
+            .then((u) => res.send({ Status: "Success" }))
+            .catch((err) => res.send({ Status: err }));
+        })
+        .catch((err) => res.send({ Status: err }));
+    }
+  });
+});
